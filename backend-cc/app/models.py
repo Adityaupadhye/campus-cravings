60a9fc523b05d9b0ed12ceec0bf3ebf55339f795
Aditya Upadhye <adityasu12@gmail.com>
2025-05-03 04:04:19 +0530

    some migration changes

--- backend-cc/app/models.py
+++ backend-cc/app/models.py
@@ -138,10 +138,14 @@ class Payment(models.Model):
         ('Completed', 'Completed'),
         ('Failed', 'Failed'),
     ]
-    order = models.OneToOneField(ActiveOrder,on_delete=models.CASCADE,related_name='payment')
-    status= models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES, default='Pending')
+    order = models.ForeignKey(ActiveOrder,on_delete=models.CASCADE,related_name='payment')
+    status = models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES, default='Pending')
     payment_time = models.DateTimeField(auto_now_add=True)
     transaction_id = models.CharField(max_length=50,unique=True,blank=True, null=True)
+
+    # Timestamp fields
+    created_at = models.DateTimeField(auto_now_add=True)  # Set once when record is created
+    updated_at = models.DateTimeField(auto_now=True)      # Automatically updated on each save
 
     def __str__(self):
         return f"Payment for Order {self.order.id} : {self.status}"
